71533a66f7bbb369829f9e274d208511896967d8
CFCode-git <435184766@qq.com>
2020-01-05 17:04:41 +0800

    build

---
+++ build/precache-manifest.f82755b7b9183c47f401561660f9611b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7fa90acc281396704fd531fc9df507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "864ffd98c86f97c13573",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "09dbcd163919ecae57ad",
+    "url": "/static/js/2.b6d1e5aa.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b6d1e5aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "864ffd98c86f97c13573",
+    "url": "/static/js/main.6fab850f.chunk.js"
+  },
+  {
+    "revision": "c0822a49f7e6eddb8968",
+    "url": "/static/js/runtime-main.9dbb812f.js"
+  }
+]);
